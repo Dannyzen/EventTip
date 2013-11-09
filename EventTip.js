04ce59dfd2fbd820e0e868ac40d5f3fbe276e086
Eric Weiser <eweiser@sailthru.com>
2013-11-09 17:39:32 -0500

    so far so good

--- EventTip.js
+++ EventTip.js
@@ -28,10 +28,43 @@ if (Meteor.isClient) {
       return Games.findOne({_id:Session.get("currentGame")}).name;
     }
   });
+
+  Template.create_game.helpers({
+    name: function() {
+      return Games.findOne({_id:Session.get("currentGame")}).name;
+    },
+    getReqs: function() {
+      return Games.findOne({_id:Session.get("currentGame")}).reqs;
+    }
+  });
+
+  Template.create_game.events({
+    'keyup input.game_num_input': function(evt) {
+        if (evt.keyCode == 13){
+            var num = $('.game_num_input').val().trim();
+            Games.update({_id:Session.get("currentGame")}, {$set:{num_people:num}});
+            console.log('update');
+            $('input.game_num_input').val(num);
+        }
+    },
+
+    'keyup input.game_req_input': function(evt) {
+        if (evt.keyCode == 13){
+            var req = $('.game_req_input').val().trim();
+            Games.update({_id:Session.get("currentGame")}, {$addToSet: {reqs: {name: req}}});
+            console.log('update');
+        }
+    },
+
+    'keyup input.game_desc_input': function(evt) {
+        if (evt.keyCode == 13){
+            var desc = $('.game_desc_input').val().trim();
+            Games.update({_id:Session.get("currentGame")}, {$set:{desc: desc}});
+            console.log('update');
+        }
+    }
+  });
 }
 
 if (Meteor.isServer) {
-  Meteor.publish('games', function () {
-    return Lists.find();
-  });
 }
